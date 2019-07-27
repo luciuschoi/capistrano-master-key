85eca90c88a1efcf004dc0d024f0e85291a7d289
Lucius Choi <lucius.choi@gmail.com>
2019-07-27 23:47:44 +0900

    deleted capistrano-master-key.rb

--- lib/capistrano-master-key.rb
+++
@@ -1,1 +0,0 @@
-load File.expand_path('../tasks/master_key.rake', __FILE__)
--- lib/capistrano/master_key.rb
+++ lib/capistrano/master_key.rb
@@ -1,3 +1,5 @@
 require "capistrano/master_key/paths"
 require "capistrano/master_key/helpers"
+require "capistrano/master_key/version"
+
 load File.expand_path("../tasks/master_key.rake", __FILE__)
